b8e95b5d7f5c0c704424dc199fb9ca3d05f6e4fe
Dimitri <Dimitri16h@gmail.com>
2018-05-25 12:48:12 -0400

    created dist files, added Procfile

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
-	res.send('Working');
-});
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(5000, function() {
-	console.log('Listening on port 5000!');
+app.listen(port, function(){
+	console.log('I am listening on port ' + port + '!');
 });
